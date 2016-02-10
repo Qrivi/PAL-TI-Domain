eed5b7cced9004cbe978ebc489c6c82147dc6e4e
Koen_DE <koen.demonie1992@gmail.com>
2016-02-11 00:53:36 +0100

    TutorJPADAOTest: property datatypefactory added DatabaseImage.xml: re-ordered to pass constraints pom: dependency comments added

--- src/test/java/be/peerassistedlearningti/dao/jpa/TutorJPADAOTest.java
+++ src/test/java/be/peerassistedlearningti/dao/jpa/TutorJPADAOTest.java
@@ -5,41 +5,57 @@ import be.peerassistedlearningti.model.Course;
 import be.peerassistedlearningti.model.Student;
 import be.peerassistedlearningti.model.Tutor;
 import org.dbunit.Assertion;
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.database.DatabaseConfig;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.datatype.DefaultDataTypeFactory;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.ext.mysql.MySqlDataTypeFactory;
+import org.dbunit.ext.postgresql.PostgresqlDataTypeFactory;
 import org.dbunit.operation.DatabaseOperation;
 import org.hibernate.internal.SessionImpl;
-import org.junit.Assert;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.junit.Assert.assertNotNull;
+
 
 public class TutorJPADAOTest {
     protected static EntityManagerFactory entityManagerFactory;
     protected static EntityManager entityManager;
     protected static IDatabaseConnection connection;
+    protected static IDataSet cleanDataset;
     protected static TutorJPADAO tutorJPADAO;
     protected static StudentJPADAO studentJPADAO;
     protected static CourseJPADAO courseJPADAO;
 
-    @Before
-    public void setup() throws Exception{
+    @BeforeClass
+    public static void init() throws MalformedURLException, DatabaseUnitException, FileNotFoundException {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory( "PAL" );
         entityManager = entityManagerFactory.createEntityManager();
         connection = new DatabaseConnection(((SessionImpl) (entityManager.getDelegate())).connection());
+        connection.getConfig().setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new MySqlDataTypeFactory());
+        FlatXmlDataSetBuilder flatXmlDataSetBuilder = new FlatXmlDataSetBuilder();
+        flatXmlDataSetBuilder.setColumnSensing(true);
+        cleanDataset = flatXmlDataSetBuilder.build(new FileInputStream("src/test/resources/database_images/DatabaseImage.xml"));
 
-        IDataSet dataset = new FlatXmlDataSetBuilder().build(new File("database_images/DatabaseImage.xml"));
-        DatabaseOperation.CLEAN_INSERT.execute(connection,dataset);
-
+        //FIXME kan maar 1 connectie met db tergelijk openen.
         tutorJPADAO = new TutorJPADAO();
         tutorJPADAO.setEntityManagerFactory(entityManagerFactory);
         studentJPADAO = new StudentJPADAO();
@@ -48,13 +64,34 @@ public class TutorJPADAOTest {
         courseJPADAO.setEntityManagerFactory(entityManagerFactory);
     }
 
+    @Before
+    public void setup() throws DatabaseUnitException, SQLException {
+        DatabaseOperation.CLEAN_INSERT.execute(connection,cleanDataset);
+    }
+
+    @AfterClass
+    public static void breakdown() throws SQLException {
+        connection.close();
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+
     @Test
     public void testAddTutor()throws Exception{
+
+
+        Student s = new Student( "Koen", "paswoord", "koen1992@hotmail.com", true );
+        System.out.println("Student:"+s.getName());
+        s = studentJPADAO.add( s );
+        System.out.println("Student:"+s.getName());
+        s = studentJPADAO.getLast();
+        System.out.println("Student:"+s.getName());
+
         Student student = studentJPADAO.getById(2);
-        Assert.assertNotNull(student);
+        assertNotNull(student);
 
         Course course = courseJPADAO.getById(1);
-        Assert.assertNotNull(course);
+        assertNotNull(course);
 
         Set<Course> courses = new HashSet<Course>();
         courses.add(course);
@@ -63,7 +100,7 @@ public class TutorJPADAOTest {
         tutorJPADAO.add(newTutor);
 
         //get expected result
-        IDataSet expectedDataset = new FlatXmlDataSetBuilder().build(new File("database_images/expected_results/tutor/addTutorResultImage.xml"));
+        IDataSet expectedDataset = new FlatXmlDataSetBuilder().build(new File("src/test/resources/database_images/expected_results/tutor/addTutorResultImage.xml"));
         ITable expectedTutorTable = expectedDataset.getTable("table");
         //get actual result
         IDataSet actualDataset = connection.createDataSet();
