33b2e0618480d06797f7a64de2686611f6c5bdc4
David Op de Beeck <davidopdebeeck@hotmail.com>
2016-02-11 00:25:50 +0100

    Added BookingDAO and BookingJPADAO

---
+++ src/main/java/be/peerassistedlearningti/dao/BookingDAO.java
@@ -0,0 +1,7 @@
+package be.peerassistedlearningti.dao;
+
+import be.peerassistedlearningti.common.dao.CRUDDAO;
+import be.peerassistedlearningti.model.Booking;
+
+public interface BookingDAO extends CRUDDAO<Integer, Booking>
+{}
