ddf5342780131525e8f1b8d3e68fd42a37d056f5
David Op de Beeck <davidopdebeeck@hotmail.com>
2016-02-11 20:31:56 +0100

    Added documentation to BookingDAO and LessonDAO

--- src/main/java/be/peerassistedlearningti/dao/jpa/LessonJPADAO.java
+++ src/main/java/be/peerassistedlearningti/dao/jpa/LessonJPADAO.java
@@ -4,6 +4,12 @@ import be.peerassistedlearningti.common.dao.CRUDJPADAO;
 import be.peerassistedlearningti.dao.LessonDAO;
 import be.peerassistedlearningti.model.Lesson;
 
+/**
+ * Class for Lesson specific DAO operations via JPA
+ *
+ * @see Lesson
+ * @see CRUDJPADAO
+ */
 public class LessonJPADAO extends CRUDJPADAO<Integer, Lesson> implements LessonDAO
 {
 
