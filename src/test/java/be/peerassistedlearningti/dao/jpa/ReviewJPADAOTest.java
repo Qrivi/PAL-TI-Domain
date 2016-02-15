9a62c9c0a66dd0fb4c98eee409d9fa568a7e0a89
Koen_DE <koen.demonie1992@gmail.com>
2016-02-15 19:44:06 +0100

    Created main structure

---
+++ src/test/java/be/peerassistedlearningti/dao/jpa/ReviewJPADAOTest.java
@@ -0,0 +1,39 @@
+package be.peerassistedlearningti.dao.jpa;
+
+/**
+ * Class used to test ReviewJPADAO
+ */
+public class ReviewJPADAOTest extends JPADAOTest {
+
+
+    public ReviewJPADAOTest(){super("PAL");}
+    @Override
+    public void testAdd() {
+
+    }
+
+    @Override
+    public void testUpdate() {
+
+    }
+
+    @Override
+    public void testRemove() {
+
+    }
+
+    @Override
+    public void testGetById() {
+
+    }
+
+    @Override
+    public void testGetAll() {
+
+    }
+
+    @Override
+    public void testGetLast() {
+
+    }
+}
