d70e57cc8acd0a6614c341c379a2b896054e9b70
Koen_DE <koen.demonie1992@gmail.com>
2016-02-15 20:54:17 +0100

    Written test cases

--- src/test/java/be/peerassistedlearningti/dao/jpa/ReviewJPADAOTest.java
+++ src/test/java/be/peerassistedlearningti/dao/jpa/ReviewJPADAOTest.java
@@ -1,6 +1,18 @@
 package be.peerassistedlearningti.dao.jpa;
 
+import be.peerassistedlearningti.model.*;
+import org.hibernate.exception.ConstraintViolationException;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Class used to test ReviewJPADAO
@@ -8,6 +20,11 @@ import org.junit.Before;
 public class ReviewJPADAOTest extends JPADAOTest {
 
     private static ReviewJPADAO reviewJPADAO;
+    private Student student, student1;
+    private Lesson lesson;
+    private static final String SHORT_TEXT = "Good",
+            NORMAL_TEXT = "The lesson was great, I learned alot!",
+            LONG_TEXT="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet tortor maximus, scelerisque ex eu, bibendum purus. Aenean lectus sed." ;
     /**
      * Constructor for ReviewJPADAOTest
      */
@@ -19,37 +36,155 @@ public class ReviewJPADAOTest extends JPADAOTest {
     @Before
     public void before(){
         super.before();
+
+        // Set JPADAO
+
         reviewJPADAO = new ReviewJPADAO();
         reviewJPADAO.setEntityManagerFactory(factory);
+
+        StudentJPADAO studentJPADAO = new StudentJPADAO();
+        studentJPADAO.setEntityManagerFactory( factory );
+
+        CourseJPADAO courseJPADAO = new CourseJPADAO();
+        courseJPADAO.setEntityManagerFactory( factory );
+
+        RoomJPADAO roomJPADAO = new RoomJPADAO();
+        roomJPADAO.setEntityManagerFactory( factory );
+
+        TutorJPADAO tutorJPADAO = new TutorJPADAO();
+        tutorJPADAO.setEntityManagerFactory( factory );
+
+        LessonJPADAO lessonJPADAO = new LessonJPADAO();
+        lessonJPADAO.setEntityManagerFactory( factory );
+
+
+        // Make Student object and add to database
+        student = new Student( "Koen", "password", "koen1992@hotmail.com", UserType.NORMAL );
+        student1 = new Student( "Matthias", "password", "matthias@hotmail.com", UserType.NORMAL );
+        Student smartStudent = new Student( "David", "password", "davidopdebeeck@hotmail.com", UserType.NORMAL );
+        studentJPADAO.add( student );
+        studentJPADAO.add(smartStudent);
+        assertNotNull( student.getId() );
+        assertNotNull( smartStudent.getId() );
+
+        // Make course object and add to database & Set
+        Course course = new Course( "MBI80x", ".NET Programmeren", ".NET" );
+        course = courseJPADAO.add( course );
+        assertNotNull( course );
+
+        Set<Course> courses = new HashSet<Course>();
+        courses.add( course );
+
+        // Make room object and add to database
+        Room room = new Room( "2.25", Campus.PROXIMUS, RoomType.COMPUTER );
+        Room roomBackup = new Room( "2.26", Campus.PROXIMUS, RoomType.COMPUTER );
+        room = roomJPADAO.add( room );
+        room = roomJPADAO.add( roomBackup );
+        assertNotNull( room.getId() );
+        assertNotNull( roomBackup.getId() );
+
+        // Make tutor object and add to database
+        Tutor tutor = new Tutor(smartStudent, courses);
+        tutorJPADAO.add(tutor);
+        assertNotNull( tutor.getId() );
+
+        // Make lesson object and add to database
+        lesson = new Lesson( new Date(), "Test lesson", "Test description", 120L, course, 25, tutor, room, roomBackup );
+
+
     }
 
     @Override
     public void testAdd() {
+        Review review = new Review(NORMAL_TEXT,student,lesson,5,5,5,5);
+        reviewJPADAO.add(review);
+        assertNotNull( review.getId() );
+    }
 
+    @Test
+    public void testAddInvalidText(){
+        Review review = new Review(SHORT_TEXT,student,lesson,5,5,5,5);
+        Review review1 = new Review(LONG_TEXT,student1,lesson,5,5,5,5);
+        try{
+            reviewJPADAO.add(review);
+            reviewJPADAO.add(review1);
+            fail("Should have thrown ConstraintViolationException but did not!");
+        }catch (final ConstraintViolationException e){
+            final String msg = "Review text should be between 10 to 140 characters!";
+            assertEquals(msg, e.getMessage());
+        }
+    }
+    @Test
+    public void testAddInvalidScoreTooLow(){
+        Review review = new Review(NORMAL_TEXT,student,lesson,0,5,5,5);
+        try{
+            reviewJPADAO.add(review);
+            fail("Should have thrown ConstraintViolationException but did not!");
+        }catch (final ConstraintViolationException e){
+            final String msg = "Review content score should not be less than 1!";
+            assertEquals(msg, e.getMessage());
+        }
+    }
+    @Test
+    public void testAddInvalidScoreTooHigh(){
+        Review review = new Review(NORMAL_TEXT,student,lesson,5,5,5,11);
+        try{
+            reviewJPADAO.add(review);
+            fail("Should have thrown ConstraintViolationException but did not!");
+        }catch (final ConstraintViolationException e){
+            final String msg = "Review atmosphere score should not be greater than 10!";
+            assertEquals(msg, e.getMessage());
+        }
     }
 
     @Override
     public void testUpdate() {
-
+        Review review = new Review(NORMAL_TEXT,student,lesson,5,5,5,5);
+        reviewJPADAO.add(review);
+        assertNotNull( review.getId() );
+        review.setAtmosphereScore(10);
+        Review updated = reviewJPADAO.update(review);
+        assertEquals(review,updated);
     }
 
     @Override
     public void testRemove() {
-
+        Review review = new Review(NORMAL_TEXT,student,lesson,5,5,5,5);
+        reviewJPADAO.add(review);
+        assertNotNull( review.getId() );
+        reviewJPADAO.remove(review);
+        assertNull(reviewJPADAO.getById(review.getId()));
     }
 
     @Override
     public void testGetById() {
-
+        Review review = new Review(NORMAL_TEXT,student,lesson,5,5,5,5);
+        reviewJPADAO.add(review);
+        assertNotNull( review.getId() );
+        assertEquals(review,reviewJPADAO.getById(review.getId()));
     }
 
     @Override
     public void testGetAll() {
+        Review review = new Review(NORMAL_TEXT,student,lesson,5,7,6,4);
+        Review review1 = new Review(NORMAL_TEXT, student1,lesson,5,4,8,6);
+        reviewJPADAO.add(review); reviewJPADAO.add(review1);
+        assertNotNull( review.getId() ); assertNotNull( review1.getId() );
 
+        Collection<Review> list = reviewJPADAO.getAll();
+
+        assertNotNull( list );
+        Assert.assertEquals( 2, list.size() );
     }
 
     @Override
     public void testGetLast() {
+        Review review = new Review(NORMAL_TEXT,student,lesson,5,7,6,4);
+        Review review1 = new Review(NORMAL_TEXT, student1,lesson,5,4,8,6);
+        reviewJPADAO.add(review); reviewJPADAO.add(review1);
+        assertNotNull( review.getId() ); assertNotNull( review1.getId() );
 
+        Review last = reviewJPADAO.getLast();
+        Assert.assertEquals( review1, last );
     }
 }
