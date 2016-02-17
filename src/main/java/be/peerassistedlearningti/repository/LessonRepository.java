acc775581fd1f718c2a78782e0bcc4de9e5b096a
Matthias <matthias_hannes@hotmail.com>
2016-02-17 22:06:56 +0100

    Added Lesson filters by Course

--- src/main/java/be/peerassistedlearningti/repository/LessonRepository.java
+++ src/main/java/be/peerassistedlearningti/repository/LessonRepository.java
@@ -1,8 +1,13 @@
 package be.peerassistedlearningti.repository;
 
+import be.peerassistedlearningti.model.Course;
 import be.peerassistedlearningti.model.Lesson;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 /**
  * Interface for Lesson specific database operations
@@ -11,4 +16,15 @@ import org.springframework.stereotype.Repository;
  * @see CrudRepository
  */
 @Repository
-public interface LessonRepository extends CrudRepository<Lesson, Integer> {}
+public interface LessonRepository extends CrudRepository<Lesson, Integer> {
+    /**
+     * Gets reviews filtered by tutor
+     *
+     * @return A collection containing the reviews for that tutor
+     */
+    @Query( "SELECT l FROM Lesson l WHERE l.lesson.course = :course" )
+    Collection<Lesson> findByCourse(@Param( "course" ) Course course );
+
+    @Query( "SELECT l FROM Lesson l WHERE l.lesson.course = :course and l.lesson.date > current_time " )
+    Collection<Lesson> findUpcomingByCourse(@Param( "course" ) Course course );
+}
