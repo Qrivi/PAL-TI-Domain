ad7be1f5770dec12564ebded483bf02e4ad6bd6a
Koen_DE <koen.demonie1992@gmail.com>
2016-02-23 14:54:56 +0100

    Renamed TimelineObject to more abstract name Archivable

---
+++ src/main/java/be/peerassistedlearningti/common/model/Archivable/Archivable.java
@@ -0,0 +1,15 @@
+package be.peerassistedlearningti.common.model.archivable;
+
+import java.util.Date;
+
+
+/**
+ * Interface used to list all timeline Objects
+ */
+public interface Archivable {
+
+    /**
+     * @return The date to be used by the timeline
+     */
+    Date getArchiveDate();
+}
