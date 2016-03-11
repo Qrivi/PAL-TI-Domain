70604046e3ceafaa5cf81a31650dc1cc70b29eb6
Koen_DE <koen.demonie1992@gmail.com>
2016-03-11 18:28:32 +0100

    changed =null to IS null

--- src/main/java/be/peerassistedlearning/repository/RequestRepository.java
+++ src/main/java/be/peerassistedlearning/repository/RequestRepository.java
@@ -49,7 +49,7 @@ public interface RequestRepository extends CrudRepository<Request, Integer>
      *
      * @return A collection containing all the requests
      */
-    @Query("SELECT r FROM Request r WHERE r.lesson=null")
+    @Query("SELECT r FROM Request r WHERE r.lesson IS null ")
     Collection<Request> findAllWithoutLesson();
 
     /**
@@ -67,7 +67,7 @@ public interface RequestRepository extends CrudRepository<Request, Integer>
      * @param course The course to get the requests from
      * @return A collection containing all the requests from the specified course
      */
-    @Query("SELECT r FROM Request r WHERE :course= r.course AND r.lesson=null")
+    @Query("SELECT r FROM Request r WHERE :course= r.course AND r.lesson IS null")
     Collection<Request> findAllWithoutLesson(@Param("course") Course course);
 
     /**
